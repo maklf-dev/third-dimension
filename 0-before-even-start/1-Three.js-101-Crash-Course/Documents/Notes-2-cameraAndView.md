f0e024fdad79c49432ff73c609432c1309344a48
M.Makhmalbaf <mohammadmaklf@gmail.com>
2026-07-26 22:11:13 +0330

    Updated with OrbitControl

--- 0-before-even-start/1-Three.js-101-Crash-Course/Documents/Notes-2-cameraAndView.md
+++ 0-before-even-start/1-Three.js-101-Crash-Course/Documents/Notes-2-cameraAndView.md
@@ -27,3 +27,43 @@
 ```
 - about **near** number, we should notice that when we set the **near** and position of the camera the same number, the camera actually is **inside** the item! so we have to minus at least half of the item geometry that is in the same axis of the camera, so we can see it. for example if the camera positioned at z = 5, and item is (1,1,1); the **near** value must be <4.5 so we can see it. ( 1 / 2 = 0.5, 5 - 0.5 = 4.5)
 - with the same logic, the **far** number must be at least >=4.5
+
+## Orbit Controls
+
+- it allows the camera to orbit around the target
+- [example](https://threejs.org/examples/misc_controls_orbit.html)
+- it takes two parameter: camera and dom element
+```js
+const controls = new OrbitControls( camera, renderer.domElement );
+```
+- we can create it after canvas variable and rendering canvas, and just pass the canvas to it as the dom element:
+```js
+const canvas = document.querySelector("canvas.threejs"); // getting canvas
+const renderer = new THREE.WebGLRenderer({canvas: canvas});  // pass the canvas as the renderer
+const controls = new OrbitControls(camera, canvas); // create orbit control and pass canvas as dom element
+```
+### render loop
+
+- call render once every frame
+
+#### request animation frame
+
+- if we just put the controls in the code, as js runs, it execute `renderer.render(scene, camera);` once, and thats it! any position items had in that moment, get captured and there is no more change to them.
+- for fixing this, there are some trick that help us to execute code more than once, and times after first execute: `set timeout` , `set interval` and just creating a `loop`! but the problem is if we execute code with these, we get errors, crashes and Maximum call! because what actually happens is we call the function infinitely over and over again, and we actually don't want that!
+- each device has **Frame Rate Gap**. usually devices has refresh rate of `60` or `120`. so if we want to have fluid experience  that **syncs** with the refresh rate of the screen, we want to match the screen refresh rate to how many times we execute renderer and actually rendering the renderer that many times.
+- for rendering the renderer in needed sequences, and to sync it with different device refresh rates, we can use **Request Animation Frame**
+- in browsers, we have access to global object `window`, which has method named `requestAnimationFrame`
+- the `requestAnimationFrame` is a global function, that takes another function. and tells the computer: right before generating a new image(refresh and load a new frame) execute that function
+```js
+window.requestAnimationFrame(function)
+```
+- so in this way, we can render canvas, exactly as much as the computer refresh the frame in all devices. and we can create a loop for out renderer, but a loop that fot limited by device refresh rate, not an infinite loop:
+
+```js
+const renderLoop = () => {
+    renderer.render(scene, camera);
+    window.requestAnimationFrame(renderLoop);
+};
+
+renderLoop();
+```
--- 0-before-even-start/1-Three.js-101-Crash-Course/ThreeJsCource/2/to-the-third-two/src/main.js
+++ 0-before-even-start/1-Three.js-101-Crash-Course/ThreeJsCource/2/to-the-third-two/src/main.js
@@ -1,21 +1,54 @@
-import * as THREE from "three"; // import the whole library
+// import the whole library
+import * as THREE from "three";
 /*console.log(THREE); //to check if library is imported correctly */
 
-const scene = new THREE.Scene(); // to create the main scene
+// import orbit control addon
+import {OrbitControls} from "three/addons/controls/OrbitControls.js";
+/*console.log(OrbitControls); //to check if library is imported correctly */
 
-const cubeGeometry = new THREE.BoxGeometry(1, 1, 1); // create cube geometry: a simple cube
-const cubeMaterial = new THREE.MeshBasicMaterial({color: "green"}); // create material : just color for now
+// to create the main scene
+const scene = new THREE.Scene();
 
-const cubeMesh = new THREE.Mesh(cubeGeometry, cubeMaterial); //create new item and pass geometry and material
+// create cube geometry: a simple cube
+const cubeGeometry = new THREE.BoxGeometry(1, 1, 1);
+// create material : just color for now
+const cubeMaterial = new THREE.MeshBasicMaterial({color: "green"});
+
+//create new item and pass geometry and material
+const cubeMesh = new THREE.Mesh(cubeGeometry, cubeMaterial);
 
 scene.add(cubeMesh); // add the item to the scene
 
-const camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.1, 4.5); // create a camera and pass field of view, aspect ratio(window.innerWidth / window.innerHeight), near (anything closest than that distance you will not see), far (anything further than that distance you will not see too)
-camera.position.z = 5; // change the camera position; move it back in z axis
+/*create a camera and pass field of view, aspect ratio(window.innerWidth / window.innerHeight), near (anything closest than that distance you will not see), far (anything further than that distance you will not see too)*/
+const camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.1, 200);
+// change the camera position; move it back in z axis
+camera.position.z = 5;
 
-const canvas = document.querySelector("canvas.threejs"); // get canvas from html
+// get canvas from html
+const canvas = document.querySelector("canvas.threejs");
 
-const renderer = new THREE.WebGLRenderer({canvas: canvas}); // create a new renderer and pass canvas to it
+// create a new renderer and pass canvas to it
+const renderer = new THREE.WebGLRenderer({canvas: canvas});
 /*const renderer = new THREE.WebGLRenderer({canvas});  // it also can be this way */
-renderer.setSize(window.innerWidth, window.innerHeight); // set the size of renderer(canvas) to the browser page
-renderer.render(scene, camera); // render the canvas and pass it scene and camera
+
+// set the size of renderer(canvas) to the browser page
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+// to create orbit controls
+const controls = new OrbitControls(camera, canvas);
+
+controls.enableDamping = true; // add smooth orbiting
+controls.autoRotate = true; // active auto rotation ** need to add controls.update(); too renderLoop
+controls.dampingFactor = 0.1; // set the number of it
+
+// create a loop base on device refresh frame
+const renderLoop = () => {
+    // render the canvas and pass it scene and camera
+    renderer.render(scene, camera);
+    // is needed because of the autoRotate
+    controls.update();
+    // make this function a loop, but limited to device refresh rate, not an infinite loop
+    window.requestAnimationFrame(renderLoop);
+};
+
+renderLoop();
